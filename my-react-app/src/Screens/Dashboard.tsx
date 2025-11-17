1707d7ce7b5aa7ca457fa1ba8460d5f54d230119
Altrocks Tech Repo <119941449+AltrocksTechRepo@users.noreply.github.com>
2025-11-17 10:06:45 +0530

    import

--- my-react-app/src/Screens/Dashboard.tsx
+++ my-react-app/src/Screens/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import Carousel from '../components/ImageSlider'
 import ShoeCard from '../components/ShoesCard';
