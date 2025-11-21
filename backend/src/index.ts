7790221621a2c156c473232baa3c4fded1d5eecd
Altrocks Tech Repo <119941449+AltrocksTechRepo@users.noreply.github.com>
2025-11-21 12:28:29 +0530

    new url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    /\.netlify\.app$/,
+    "https://smmothwalk-2f0ec.web.app/",
     "https://smooth-walk.onrender.com"
   ],
   methods: ["GET", "POST", "DELETE", "PUT"],
